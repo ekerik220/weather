489db7bb11f9ef2490971406ceb53111442162ed
Evan K <ekerik220@gmail.com>
2020-04-06 16:18:57 +0900

    Updates

---
+++ precache-manifest.ac37e65bb476544614f3c33b14386184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c341434122d0415eea169c2ff3b942fd",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "5d9ed5aac4754dde1884",
+    "url": "/weather/static/css/main.bf928a15.chunk.css"
+  },
+  {
+    "revision": "3e61278895e7faabc0e2",
+    "url": "/weather/static/js/2.7fc9d52a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.7fc9d52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9ed5aac4754dde1884",
+    "url": "/weather/static/js/main.1f71df64.chunk.js"
+  },
+  {
+    "revision": "1e02fceca029e7e9b6fb",
+    "url": "/weather/static/js/runtime-main.6be21be7.js"
+  }
+]);
